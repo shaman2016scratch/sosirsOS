2197d70728d3b304190b5a76779cba5419c4b565
shaman2016scratch <g.ilkh@yandex.ru>
2026-07-20 23:36:35 +0300

    Updates

--- js/tp.js
+++ js/tp.js
@@ -1,3 +1,5 @@
 document.getElementsByClassName('top-panel')[0].innerHTML = `
     <a href="/sosirsOS/">Главная страница</a>
+    <a href="/sosirsOS/repositories">Репозитории</a>
+    <a href="/sosirsOS/versions">Версии</a>
 `
